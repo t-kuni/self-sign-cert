3c3d8e0733e25cccd25adf1c65a1c6647b53086f
t-kuni <t.kuni.dev@gmail.com>
2020-05-23 02:52:53 +0900

    update

--- src/gen.sh
+++ src/gen.sh
@@ -5,6 +5,6 @@ echo $IP
 
 envsubst < subjectnames-template.txt > subjectnames.txt
 openssl genrsa 2048 > /out/server.key
-openssl req -new -key /out/server.key -subj "/C=AU/ST=Some-State/O=Internet Widgits Pty Ltd/CN=${HOST}" > /out/server.csr
+openssl req -new -key /out/server.key -subj "/C=AU/ST=Some-State/O=Self Sign Cert/CN=${HOST}" > /out/server.csr
 openssl x509 -days 3650 -req -extfile subjectnames.txt -signkey /out/server.key < /out/server.csr > /out/server.crt
 rm subjectnames.txt
